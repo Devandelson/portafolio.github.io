f0582561f77a470e115e56ff8aa7b91f3b9e1aa9
Andelson <Devandelsongonzalez@gmail.com>
2025-03-12 21:42:59 -0400

    3/12/2025

--- JS/movimiento_frm.js
+++ JS/movimiento_frm.js
@@ -133,7 +133,7 @@ for(let i = 1; i< pages.children.length; i++){
                 mostrar_visualizador_proyecto();
                 mover_imagenes_proyecto();
                 cerrar_visualizador_pr();
-            } else if (texto_frm_seleccionado == "Sobre_Mi") {
+            } else if (texto_frm_seleccionado == "Sobre_Mí") {
                 movimiento_frm();
                 altura_contenedor_area();
                 cerrar_visualizador();
